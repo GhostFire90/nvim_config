9c67af9dc77ed2597470de01c56db4dd2e5e490e
Persephone <evan.anderson_@outlook.com>
2026-07-01 20:43:39 -0700

    yppee

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,6 +5,8 @@ vim.opt.smarttab = true
 vim.opt.tabstop = 2      -- also set this, so a literal Tab keypress inserts 2 cols
 vim.opt.softtabstop = 2  -- makes Tab/Backspace behave consistently with shiftwidth
 vim.opt.number = true
+vim.opt.termguicolors = true
+
 
 require("core.lazy_init")
 require("core.lsp")

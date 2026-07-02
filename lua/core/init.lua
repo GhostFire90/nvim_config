d1dced73f366a1be634da30efd5bdd032bc4fd19
Persephone <evan.anderson_@outlook.com>
2026-07-01 17:09:53 -0700

    init

---
+++ lua/core/init.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
+vim.opt.tabstop = 2      -- also set this, so a literal Tab keypress inserts 2 cols
+vim.opt.softtabstop = 2  -- makes Tab/Backspace behave consistently with shiftwidth
+vim.opt.number = true
+
+require("core.lazy_init")
+require("core.lsp")
+
+vim.cmd.colorscheme "catppuccin-mocha"
---
+++ lua/core/lsp.lua
@@ -0,0 +1,22 @@
+vim.lsp.enable({
+	"luals", -- lua
+	"rust_analyzer", --rust
+})
+vim.diagnostic.enable()
+vim.diagnostic.config({
+  underline=true,
+  update_in_insert=true,
+  virtual_text={
+    spacing = 2,
+    source = "if_many",
+    prefix = "!"
+  },
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    
+  }
+
+})
